91dc5a2e24e25e6bf4c6b33d86572137008376a2
Onur Polattimur <onur.polattimur@gmail.com>
2020-10-29 18:32:12 +0300

    remove unused files

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,11 +1,3 @@
-// if you checked "fancy-settings" in extensionizr.com, uncomment this lines
-
-// var settings = new Store("settings", {
-//     "sample_setting": "This is how you use Store.js to remember values"
-// });
-//var store = new Store();
-
-//example of using a message handler from the inject scripts
 chrome.extension.onMessage.addListener(
   function (request, sender, sendResponse) {
     chrome.pageAction.show(sender.tab.id);
